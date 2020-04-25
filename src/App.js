da50b06bc6ca03fca112655d53d7ccddda190b33
SalikChodhary <50224612+SalikChodhary@users.noreply.github.com>
2020-04-24 21:42:24 -0400

    design layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Grid, Button, Box, Text, Avatar} from 'grommet';
+import { Menu, Linkedin, Mail } from 'grommet-icons';
+
+
 
 function App() {
+
+  const [sidebar, setSidebar] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid
+        fill
+        rows={["auto", "flex"]}
+        columns={["auto", "flex"]}
+        areas={[
+          { name: "header", start: [0, 0], end: [1, 0] },
+          { name: "sidebar", start: [0, 1], end: [0, 1] },
+          { name: "main", start: [1, 1], end: [1, 1] }
+        ]}
+      >
+        <Box
+          gridArea="header"
+          direction="row"
+          align="center"
+          justify="between"
+          pad={{ horizontal: "medium", vertical: "small" }}
+          background="white"
+          elevation='medium'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Button onClick={() => setSidebar(!sidebar)}>
+            <Menu size='large' color='black' />
+          </Button>
+          <Text size='xlarge' color='black'>Personal Website</Text>
+          <Box gap='small' direction="row">
+            <Linkedin />  
+            <Mail />
+          </Box> 
+        </Box>
+        {sidebar && (
+          <Box
+            gridArea="sidebar"
+            background="white"
+            width="small"
+            animation={[
+              { type: "fadeIn", duration: 300 },
+              { type: "slideRight", size: "xlarge", duration: 150 }
+            ]}
+            elevation='medium'
+            align='center'
+          >
+            {["First", "Second", "Third"].map(name => (
+              <Button key={name} href="#" hoverIndicator>
+                <Box pad={{ horizontal: "medium", vertical: "small" }} >
+                  <Text color='black'>{name}</Text>
+                </Box>
+              </Button>
+            ))}
+          </Box>
+        )}
+        <Box gridArea="main" justify="center" align="center">
+          <Avatar size='xlarge' src={logo}/>
+          <Text>FUDOAUFAHOFUHFOUHFOUHFOUSHAOUFHOUFHOUFH</Text>
+        </Box>
+      </Grid>
   );
 }
 
 export default App;
+
+// import React, { Component } from "react";
+// import { render } from "react-dom";
+// import { createClient } from "contentful";
+
+
+// var client = createClient({
+//   space: "wp95wkcd3e2s",
+//   accessToken:
+//     "8h3cXJm30rnmEVv2Llvb7UugbYAbnvQ2OYqvjrgs2Tk"
+// });
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       article: []
+//     };
+//   }
+
+//   componentDidMount() {
+//     client.getEntries().then(({ items }) => {
+//       this.setState({ article: items });
+//       console.log(this.state.article)
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+        
+//         Content goes here
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Grommet, grommet } from 'grommet';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Grommet full theme={grommet}>
+      <App />
+    </Grommet>
   </React.StrictMode>,
   document.getElementById('root')
 );
